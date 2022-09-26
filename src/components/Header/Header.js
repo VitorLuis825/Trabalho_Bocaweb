e669527942f3557e45f6774469dcade1c884c2bc
Vitor Luis <64209076+VitorLuis825@users.noreply.github.com>
2022-09-26 15:28:46 -0300

    Mudança do header.js
    o puto do eslint reclama quando n tem alt prop na img

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 export const Header = () =>{
     return(
         <header>
-                <img src="logobranco.png"></img>
+                <img src="logobranco.png" alt="logo do bocaweb"></img>
                 <h1>BocaWeb FRONT</h1>
         </header>
     )
